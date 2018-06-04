e94f73e96205d1f65a67db0df4eed106e2a2d620
Alan19 <Alan19@users.noreply.github.com>
2018-06-04 12:52:38 -0400

    Using recursive directory zipper

--- date_zipper.rb
+++ date_zipper.rb
@@ -1,11 +1,65 @@
 require 'rubygems'
 require 'zip'
+require 'date'
 
-class DateZipper
-  folder = "config"
-  zipfile_name = "config" + Date.today.strftime("%Y%m%d")
+# This is a simple example which uses rubyzip to
+# recursively generate a zip file from the contents of
+# a specified directory. The directory itself is not
+# included in the archive, rather just its contents.
+#
+# Usage:
+#   directory_to_zip = "/tmp/input"
+#   output_file = "/tmp/out.zip"
+#   zf = ZipFileGenerator.new(directory_to_zip, output_file)
+#   zf.write()
 
-  Zip::File.open(zipfile_name, Zip::File::CREATE) do |zipfile|
-    zipfile.add(folder, File.join)
+class ZipFileGenerator
+  # Initialize with the directory to zip and the location of the output archive.
+  def initialize(input_dir, output_file)
+    @input_dir = input_dir
+    @output_file = output_file
+  end
+
+  # Zip the input directory.
+  def write
+    entries = Dir.entries(@input_dir) - %w(. ..)
+
+    ::Zip::File.open(@output_file, ::Zip::File::CREATE) do |zipfile|
+      write_entries entries, '', zipfile
+    end
+  end
+
+  private
+
+  # A helper method to make the recursion work.
+  def write_entries(entries, path, zipfile)
+    entries.each do |e|
+      zipfile_path = path == '' ? e : File.join(path, e)
+      disk_file_path = File.join(@input_dir, zipfile_path)
+      puts "Deflating #{disk_file_path}"
+
+      if File.directory? disk_file_path
+        recursively_deflate_directory(disk_file_path, zipfile, zipfile_path)
+      else
+        put_into_archive(disk_file_path, zipfile, zipfile_path)
+      end
+    end
+  end
+
+  def recursively_deflate_directory(disk_file_path, zipfile, zipfile_path)
+    zipfile.mkdir zipfile_path
+    subdir = Dir.entries(disk_file_path) - %w(. ..)
+    write_entries subdir, zipfile_path, zipfile
+  end
+
+  def put_into_archive(disk_file_path, zipfile, zipfile_path)
+    zipfile.get_output_stream(zipfile_path) do |f|
+      f.write(File.open(disk_file_path, 'rb').read)
+    end
   end
 end
+
+directory_to_zip = "config"
+output_file = "config#{Date.today.strftime("%Y%m%d")}.zip"
+zf = ZipFileGenerator.new(directory_to_zip, output_file)
+puts("Zipping successful, created #{output_file}")
